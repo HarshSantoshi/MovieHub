b7284d9dda1884dfc2fe878d487061dd294392bb
Harsh <harshsantoshi05@gmail.com>
2023-11-02 16:48:41 +0530

    error fixed

--- src/pages/searchResult/searchResult.jsx
+++ src/pages/searchResult/searchResult.jsx
@@ -83,6 +83,7 @@ const SearchResult = () => {
                         </>
                     ) : (
                         <span className="resultNotFound">
+                            
                             Sorry, Results not found!
                         </span>
                     )}
